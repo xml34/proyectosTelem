9cb043df065df168d3a5611a5af9dfc44eae8d34
root <root@proxy1k8s.dis.eafit.edu.co>
2018-09-09 16:50:16 -0400

    Qa terminado

--- proyecto2/routes/inicio.js
+++ proyecto2/routes/inicio.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var mongo = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
+var antLat="";
+var antLong="";
+var antEm="";
 
 var url = 'mongodb://10.131.137.224:27017/test';
 
@@ -12,30 +15,41 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/insert', function(req, res, next) {
+  if(antEm == JSON.stringify(req.body.email)){
+    console.log("Emails Iguales");
+  } else {
+    console.log("Emails diferentes");
+  }
+  
+  if(antLat != JSON.stringify(req.body.Latitud) && antLong != JSON.stringify(req.body.Longitud)){
+    antLat = JSON.stringify(req.body.Latitud);
+    antLong = JSON.stringify(req.body.Longitud);
+    antEm = JSON.stringify(req.body.email);
 
-  var antLat = JSON.stringify(req.body.Latitud);
-  var antLong = JSON.stringify(req.body.Longitud);
-  var antEm = JSON.stringify(req.body.email);
-
-  console.log("MENSAJE MIO");
-  console.log(antEm);
-  var item = {
-    latitud: antLat,
-    longitud: antLong,
-    email: antEm
-  };
+    console.log("MENSAJE MIO");
+    console.log(antEm);
+    var item = {
+      latitud: req.body.Latitud,
+      longitud: req.body.Longitud,
+      email: req.body.email
+    };
 
 
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('data').insertOne(item, function(err, result) {
+    mongo.connect(url, function(err, db) {
+      assert.equal(null, err);
+      db.collection('data').insertOne(item, function(err, result) {
       assert.equal(null, err);
       console.log('Item inserted');
       db.close();
     });
   });
 
+  }else{
+    console.log("No guardó");
+  }
   res.redirect('/inicio');
+
+
 });
 
 
